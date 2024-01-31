ebaaac6198f117cefeeb4b979425fbf50f9f83dd
adityaundre <adityarundre@gmail.com>
2024-01-31 19:16:05 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import React from 'react';
+import { useState,useEffect } from 'react';
+
+function App() {
+const[value,setValue]=useState(0)
+const[increment,setIncrement]=useState(0);
+const[decrement,setDecrement]=useState(0);
+
+const increase=()=>{
+  setValue(value+3);
+
+}
+const decrease=()=>{
+  setValue(value-1)
+}
+
+  return (
+    <div className="App">
+      <h2>Counter App</h2>
+      <p>Count: {value}</p>
+      <button onClick={increase}>Increment</button>
+      <button onClick={decrease}>decrement</button>
+
+    </div>
+  );
+}
+
+export default App;
