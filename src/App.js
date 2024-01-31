338a7cb871d1b827c5314bba60daaeec9f6ba361
adityaundre <adityarundre@gmail.com>
2024-01-31 19:46:56 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import { useState,useEffect } from 'react';
 
 function App() {
 const[value,setValue]=useState(0)
-const[increment,setIncrement]=useState(0);
-const[decrement,setDecrement]=useState(0);
+
 
 const increase=()=>{
   setValue(value+1);
@@ -20,7 +19,7 @@ const decrease=()=>{
       <h2>Counter App</h2>
       <p>Count: {value}</p>
       <button onClick={increase}>Increment</button>
-      <button onClick={decrease}>decrement</button>
+      <button onClick={decrease}>Decrement</button>
 
     </div>
   );
